a5836aeccc8cbf1bda1da4b1c9e49cd1eb521afd
hanmillee <94hanmil@gmail.com>
2021-12-30 00:43:42 +0900

    footer에 hit수 넣기

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,13 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>
+        <div>Copyright 2021 {config.author} </div>
+        <div>ㅤ</div>
+        <a href='https://hits.seeyoufarm.com'>
+          <img src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhmdev.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true' />
+        </a>
+      </div>
 
       {hasMounted ? (
         <div className={styles.settings}>
