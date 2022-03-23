014c840df0967783c6b8e207d098b4c0252f9b7d
hanmillee <94hanmil@gmail.com>
2022-03-24 01:10:04 +0900

    total hit 가운데 정렬

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,9 +27,17 @@ export const Footer: React.FC<{
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-        <div>
+        <div
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
           <a href='https://hits.seeyoufarm.com'>
-            <img src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhmdev.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Total hits&edge_flat=true' />
+            <img
+              style={{ display: 'block', margin: '0px auto' }}
+              src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhmdev.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Total hits&edge_flat=true'
+            />
           </a>
           <div>Copyright 2022 {config.author} </div>
         </div>
