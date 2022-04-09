6d6e3cfbc6e962937ac4cad33aca9efbe4395521
hanmillee <94hanmil@gmail.com>
2022-04-09 15:51:51 +0900

    nav bar custom

--- site.config.ts
+++ site.config.ts
@@ -60,16 +60,16 @@ export default {
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'a32a59aec23c41e08452757e94d96418'
+    },
+    // {
+    //   title: 'Contact',
+    //   pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    // }
+  ]
 }
