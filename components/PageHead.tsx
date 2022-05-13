7be89b38c297e5025daabe80fffae0acc183dc82
hanmillee <94hanmil@gmail.com>
2022-05-13 11:21:48 +0900

    GA 정상화

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -51,7 +51,7 @@ export const PageHead: React.FC<
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', ${config.GAId}, {
+        gtag('config', '${config.GAId}', {
           page_path: window.location.pathname,
         });
         `
