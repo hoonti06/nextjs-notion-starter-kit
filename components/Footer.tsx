c4d96ed2884d7df366340f432aa171d84cd7beaf
hanmillee <94hanmil@gmail.com>
2023-02-20 00:39:35 +0900

    copyright 2022 to 2023

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -48,7 +48,7 @@ export const FooterImpl: React.FC = () => {
               src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhmdev.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Total hits&edge_flat=true'
             />
           </a>
-          <div>Copyright 2022 {config.author} </div>
+          <div>Copyright 2023 {config.author} </div>
         </div>
       </div>
 
