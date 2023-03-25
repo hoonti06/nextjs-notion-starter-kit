7f6ee1cc9c4f014cc92bc76ac7d5afd0ec350a71
hanmillee <94hanmil@gmail.com>
2023-03-26 00:41:44 +0900

    tmp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,13 +11,13 @@ import posthog from 'posthog-js'
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-// import 'styles/global.css'
+import 'styles/global.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
-// import 'styles/prism-theme.css'
+import 'styles/prism-theme.css'
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
